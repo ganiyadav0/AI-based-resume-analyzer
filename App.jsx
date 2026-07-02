dc185d250a28ad1ad26d70721ca81f238c50ec8c
Ganesh jakkula <127415423+ganiyadav0@users.noreply.github.com>
2026-07-02 22:12:09 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import HomePage from "./pages/Home.jsx";
+function App() {
+
+
+  return (
+    <>
+      <HomePage />
+    </>
+    
+  )
+}
+
+export default App
